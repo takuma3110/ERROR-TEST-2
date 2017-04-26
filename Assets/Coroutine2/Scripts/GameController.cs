66eae77eb45f9fca95c6816b3ae314021034c6d3
Takuma Saito <takuma_saito@TakumanoMacBook-Pro.local>
2017-04-26 14:19:27 +0900

    Non error project

---
+++ Assets/Coroutine2/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public GameObject enemy;
+    public GameObject stage;
+    public GameObject player;
+    GameObject[] enemies;
+    float stageWidth;
+    float stageHeight;
+    Player playerScript;
+
+    void Start () {
+        stageWidth = stage.GetComponent<Renderer> ().bounds.size.x;
+        stageHeight = stage.GetComponent<Renderer> ().bounds.size.z;
+        playerScript = player.GetComponent<Player> ();
+
+        for(int n = 0; n < 5; n++) {
+            Instantiate(enemy, new Vector3 (Random.Range (-stageWidth / 2, stageWidth / 2), 1, Random.Range (-stageHeight / 2, stageHeight / 2)), Quaternion.identity);
+        }
+    }
+
+    void Update () {
+        if (playerScript.getDestroy == true) {
+            StartCoroutine ("GenerateEnemy");
+            playerScript.getDestroy = false;
+        }
+    }
+
+    IEnumerator GenerateEnemy(){
+        yield return new WaitForSeconds (3.0f);
+        Instantiate (enemy, new Vector3(Random.Range(-stageWidth/2,stageWidth/2),1,Random.Range(-stageHeight/2,stageHeight/ 2)),Quaternion.identity);
+    }
+}
---
+++ Assets/Coroutine2/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public bool getDestroy = false;
+
+    void OnCollisionEnter(Collision  other){
+        if (other.gameObject.tag == "Enemy") {
+            Destroy (other.gameObject);
+            getDestroy = true;
+        }
+    }
+}
