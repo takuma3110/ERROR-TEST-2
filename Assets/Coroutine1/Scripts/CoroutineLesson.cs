b11d248525665866e13c3a6f11a9b1542b52b73d
Takuma Saito <takuma_saito@TakumanoMacBook-Pro.local>
2017-04-26 14:04:05 +0900

    init

---
+++ Assets/Coroutine1/Scripts/CoroutineLesson.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineLesson : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine ("MoveSphere");
+    }
+
+    IEnumerator MoveSphere() {
+        for(int f = 10; f>= 0; f--) {
+            gameObject.transform.position = new Vector3 (f * 0.1f, 1, 1);
+            yield return new WaitForSeconds (0.5f);
+            print (Time.time);
+        }
+    }
+}
